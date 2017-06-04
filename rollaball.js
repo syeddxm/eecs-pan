6011070b9246381fe431ad3e5ec2bd962d840a9e
syeddxm <syed@dxm.to>
2017-06-04 14:57:11 -0400

    6.1

--- rollaball.js
+++ rollaball.js
@@ -1,18 +1,20 @@
 function init() {
-  document.getElementById("count").innerHTML = 0;
+//  document.getElementById("count").innerHTML = 0;
   if(window.DeviceOrientationEvent) {
-    document.getElementById("log").innerHTML = "Supports orientation events";
+    document.getElementById("text").innerHTML = "Supports orientation events";
     window.addEventListener("deviceorientation", orientationCallback, true);
   } else {
-    document.getElementById("log").innerHMTL = "No orientation event support";
+    document.getElementById("text").innerHMTL = "No orientation event support";
   }
 }
 
+/*
 function orientationCallback(eventData) {
   document.getElementById("count").innerHTML = Number( document.getElementById("count").innerHTML) + 1;
   document.getElementById("tilt").innerHTML = eventData.gamma;
   document.getElementById("pitch").innerHTML = eventData.beta;
 }
+*/
 
 alert("javascript loaded");
 window.onload = init;
